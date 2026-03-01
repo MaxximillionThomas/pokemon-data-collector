d167f3d429a238351f00ff3cffb1f4fdbf47cfe8
Maxximillion Thomas <maxxthomas123@gmail.com>
2026-03-01 18:37:23 -0500

    Integration of cloud data
    Configured an S3 bucket for private cloud storage of Pokemon JSON data.
    
    Set up CloudFront distribution with Origin Access Control (OAC) for secure, HTTPS-only data delivery.
    
    Configured dynamic retrieval of Pokemon data from CloudFront through useEffect with Fetch.
    
    Added a loading state to allow for conditional rendering of PokemonCards when handling async data retrieval.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,72 @@
  */
 
 import { PokemonCard } from './components/PokemonCard';
+import { useEffect, useState } from 'react';
 
 function App() {
-  // Quick test
-  const bulbasaur = {
-    id: 1,
-    name: "Bulbasaur",
-    types: ["grass"],
-    sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-  }
+  // Control which Pokemon are to populate the overview page
+  const [pokemon, setPokemon] = useState(null);
+
+  // Keep the user updated on the loading status of the application
+  const [loading, setLoading] = useState(true);
+
+  // On first load, ALL Pokemon should be visible 
+  // HOWEVER, right now we only have the Card component set up, and no map function yet to an array of PokemonCards (!!! come back !!!)
+  useEffect(() => {
+    // Target the CloudFront distribution for the S3 bucket (only bulbasaur - temporarily manually created - right now per the above)
+    const CLOUDFRONT_URL = 'https://d1xb64xlhesy7f.cloudfront.net/bulbasaur.json';
+
+    // Fetch the data from cloud storage
+    fetch(CLOUDFRONT_URL)
+      // If the HTTP response is ok, return it for data handling
+      .then((response) => {
+        if (!response.ok) {
+          // Create a custom error message and throw it to the 'catch' block
+          const error = new Error(`Status ${response.status}`);
+          error.type = 'HTTP_ERROR';
+          throw error;
+        }
+
+        return response.json();
+      })
+
+      // Use the response data (Pokemon overview JSON data) to set the Pokemon state object
+      .then ((data) => {
+        setPokemon(data);
+      })
+
+      // The response was NOT ok
+      .catch((error) => {
+        // Handle HTTP status errors (e.g., 404, 500) thrown in 'then' block
+        if (error.type === 'HTTP_ERROR') {
+          console.log(`API request failed with status code: ${error.message}.`);
+        // Handle connectivity, DNS, or security/CORS failures
+        } else {
+          console.log('Network or CORS issue. Check CloudFront/S3 policy.');
+        }
+      });
+
+      // Loading message shown until state object set to false
+      setLoading(false);
+
+  // Run only on first load of the web application
+  }, []);
 
   return (
     <div className="container mt-5">
       <h1>Pokémon Data Collector</h1>
-      {/* Quick test */}
-      <PokemonCard pokemon={bulbasaur}/>
+
+      {/* Quick test - to replace PokemonCard with PokemonList (array of PokemonCards) */}
+      {loading ? (
+      // Display loading message until fetch complete
+        <p>Fetching data...</p>
+      // Fetch success
+      ) : pokemon ? (
+        <PokemonCard pokemon={pokemon}/>
+      // Fetch failure
+      ) : (
+        <p>Failed to fetch Pokemon data.</p>
+      )}
     </div>
   );
 }
