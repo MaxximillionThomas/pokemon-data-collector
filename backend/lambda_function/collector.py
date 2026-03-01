8ce97727719b939dbc60c485f7e262347267c3e7
Maxximillion Thomas <maxxthomas123@gmail.com>
2026-02-28 21:36:18 -0500

    PokeApi overview data
    Set up a function for communicating with PokeApi to obtain the overview data (id, name, types, sprite) for a given Pokemon.
    
    Verified functionality by running the python file with sample data.

--- backend/lambda_function/collector.py
+++ backend/lambda_function/collector.py
@@ -6,3 +6,49 @@ Description:    Defines the primary data collection function and API interaction
 Author:         Maxximillion Thomas
 Date:           February 28, 2026
 """
+
+import requests
+
+def fetch_pokemon_overview(pokemon):
+    """
+    Fetch and filter the overview data for a specific Pokemon.
+    """
+    # Inject the target pokemon into the pokeapi link
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}"
+
+    # Request the data, attempting to parse the response and map it to desired pairs
+    try:
+        # Obtain the data
+        response = requests.get(url)
+        response.raise_for_status()
+        raw_data = response.json()
+
+        # Map the data
+        overview_data = {
+            "id": raw_data["id"],
+            "name": raw_data["name"],
+            "types": [t["type"]["name"] for t in raw_data["types"]],
+            "sprite": raw_data["sprites"]["front_default"]
+        }
+        return overview_data        
+
+    # If unsuccessful, print the error message to the terminal
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
+    
+
+
+# ##########
+# Quick test
+# ##########
+
+print("Attempting to fetch overview data for Mew...")
+mew_overview = fetch_pokemon_overview("mew")
+
+if (mew_overview):
+    print("Success!")
+    print(mew_overview)
+
+else:
+    print("Failure")
